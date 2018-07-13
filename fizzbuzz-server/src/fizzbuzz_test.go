ea170f2d1f430dbc00f67b5df142e748dd6a5082
ashwath <ashwathps@users.noreply.github.com>
2018-07-13 14:37:31 +1000

    Channel was overkill

--- fizzbuzz-server/src/fizzbuzz_test.go
+++ fizzbuzz-server/src/fizzbuzz_test.go
@@ -7,10 +7,9 @@ import (
 )
 
 func TestFizzBuzz15(t *testing.T) {
-	have := ""
-	for got := range FizzBuzz(int64(15)) {
-		have += strings.Replace(got, "\n", "", -1)
-	}
+	have := FizzBuzz(int64(15))
+
+	have = strings.Replace(have, "\n", "", -1)
 
 	fmt.Println(have)
 	want := "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz"
